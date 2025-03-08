9b25a757e4cd10b9de5ae3cfbcbbf842ba031484
Abhishek Malvadkar <abhi@dev.com>
2025-03-08 20:14:11 +0530

    Initial commit

---
+++ src/main/java/com/amalvadkar/commons/utils/AmCommonsUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.amalvadkar.commons.utils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmCommonsUtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmCommonsUtilsApplication.class, args);
+	}
+
+}
